ebf74c36246ceb9152540a9da09e390230be10c6
Jyothi seeram <159920254+Jyothiseeram@users.noreply.github.com>
2025-06-15 10:10:14 +0530

    [1432] [Time Beats: 100.00%] [Memory Beats: 88.27%] - LeetPush

---
+++ Max-Difference-You-Can-Get-From-Changing-an-Integer.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        s = str(num)
+        max_val = int(s)
+        min_val = int(s)
+
+        for x in '0123456789':
+            for y in '0123456789':
+                if x == y:
+                    continue
+                replaced = s.replace(x, y)
+                if replaced[0] == '0':
+                    continue
+                val = int(replaced)
+                max_val = max(max_val, val)
+                min_val = min(min_val, val)
+
+        return max_val - min_val
+ 
+
+        
