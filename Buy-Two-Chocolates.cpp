fd78dce18056b2a6f615be7517cba19d0a2a4f69
Jyothi seeram <159920254+Jyothiseeram@users.noreply.github.com>
2025-10-11 13:42:29 +0530

    [2706] [Time Beats: 100.00%] [Memory Beats: 30.87%] - LeetPush

---
+++ Buy-Two-Chocolates.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int buyChoco(vector<int>& prices, int money) {
+        sort(prices.begin(),prices.end());
+        int cost=prices[0]+prices[1];
+        if(money>=cost)
+        {
+            return money-cost;
+        }
+        else{
+            return money;
+        }
+    }
+};
