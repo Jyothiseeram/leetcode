0d70cb105fc26a494a9ae3a3df49af07acc90056
Jyothi seeram <159920254+Jyothiseeram@users.noreply.github.com>
2025-12-05 11:40:43 +0530

    [3432] [Time Beats: 100.00%] [Memory Beats: 53.79%] - LeetPush

--- Count-Partitions-with-Even-Sum-Difference.cpp
+++ Count-Partitions-with-Even-Sum-Difference.cpp
@@ -1,23 +1,22 @@
-class Solution {
-public:
-    int countPartitions(vector<int>& nums) {
-        int totalsum=0;
-        int count=0;
-        for(int i:nums)
-        {
-            totalsum+=i;
-        }
-        int left=0;
-        for(int i=0;i<nums.size()-1;i++)
-        {   
-            left+=nums[i];
-            int right=abs(totalsum-left);
-            if((left-right)%2==0)
-            {
-                count++;
-            }
-           
-        }
-        return count;
-    }
-};
+1class Solution {
+2public:
+3    int countPartitions(vector<int>& nums) {
+4        int left=0,cnt=0;
+5        int totalsum=0,res;
+6        for(int x:nums)
+7        {
+8              totalsum+=x;
+9        }
+10        for(int i=0;i<nums.size()-1;i++)
+11        {
+12            left+=nums[i];
+13           int right=totalsum-left;
+14            res=right-left;
+15            if(res%2==0)
+16            {
+17                cnt++;
+18            }
+19        }
+20        return cnt;
+21    }
+22};
