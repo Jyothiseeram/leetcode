729d2ff218283def6c53460803207b94840371cc
Jyothi seeram <159920254+Jyothiseeram@users.noreply.github.com>
2025-12-07 08:16:54 +0530

    [1523] [Time Beats: 20.70%] [Memory Beats: 69.29%] - LeetPush

--- Count-Odd-Numbers-in-an-Interval-Range.cpp
+++ Count-Odd-Numbers-in-an-Interval-Range.cpp
@@ -1,14 +1,12 @@
-class Solution {
-public:
-    int countOdds(int low, int high) {
-        int cnt=0;
-        for(int i=low;i<=high;i++)
-        {
-            if(i%2==1)
-            {
-                cnt++;
-            }
-        }
-        return cnt;
-    }
-};
+1class Solution {
+2public:
+3    int countOdds(int low, int high) {
+4        int cnt = 0;
+5        for (int i = low; i <= high; i++) {
+6            if (i % 2 != 0) {
+7                cnt++;
+8            }
+9        }
+10        return cnt;
+11    }
+12};
